0b5e70e24ca43feb30253930dfe613c696933be9
fwefew123 <artem.andreev200012@mail.ru>
2023-11-24 21:52:12 +0600

    version 1.0.0

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
-let text = document.getElementById('main_text')
-var firstButton = document.getElementById('button_rock')
-let secondButton = document.getElementById('button_scissors')
-let thirdButton = document.getElementById('button_paper')
-var user = document.getElementById('user')
-var comp = document.getElementById('comp')
-var result = document.getElementById('result')
+var firstButton = document.getElementById('firstButton')
+var userResult = document.getElementById('userResult')
+var compResult = document.getElementById('compResult')
+var finalResult = document.getElementById('finalResult')
 
 var choice = ['rock', 'scissors', 'paper']
 
-var randomChoice = ''
+var randomChoice;
 
 function setRandom() {
-    randomChoice = choice[Math.floor(Math.random() * choice.length)]
-}
-
-function disabledButton() {
-    if (firstButton) {
-        disabled = true
+    randomChoice = Math.floor(Math.random() * choice.length)
+    switch(randomChoice) {
+        case 0: 
+        compValue = 'rock'
+        break;
+        case 1: 
+        compValue = 'scissors'
+        break;
+        case 2: 
+        compValue = 'paper'
+        break;
     }
 }
 
-firstButton.addEventListener('click', function(){
-    setRandom()
-    result.innerHTML += randomChoice
-})  
+    
+function userValue() {
+    let user = choice
+    let userRock = choice[0]
+    let userScissors = choice[1]
+    let userPaper = choice[2]
+}
 
+
+function winner() {
+    switch(finalResult){
+        case user == compValue:
+            return 'Ничья!'
+        case user = 'rock':
+            comp = 'scissors'
+            return 'Вы выиграли!'
+        case userScissors = 'scissors':
+            comp = 'paper'
+            return 'Вы выиграли!'
+        case userPaper = 'paper':
+            comp = 'rock'
+            return 'Вы выиграли!'
+        default:
+            return 'Вы проиграли :('
+    }
+}
+
+
+firstButton.addEventListener('click', function(){ 
+    userResult.innerHTML = userValue()
+})
